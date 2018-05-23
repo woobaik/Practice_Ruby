66d988ac98d2fceb5d0884c2d99a1cca013e1c4e
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-22 17:23:17 -0700

    Finished enumerable part

--- W1D1/enumerables.rb
+++ W1D1/enumerables.rb
@@ -33,8 +33,31 @@ class Array
     end
     arr
   end
+#
+#   a = [1, 2, 3]
+# p a.my_select { |num| num > 1 } # => [2, 3]
+# p a.my_select { |num| num == 4 } # => []
+
+
+  def my_any?(&prc)
+    self.my_each do |num|
+      return true if prc.call(num)
+    end
+    false
+  end
+
+  def my_all?(&prc)
+    new_arr = []
+    self.my_each do |num|
+      new_arr << num if prc.call(num)
+    end
+      self == new_arr
+  end
+
 
   a = [1, 2, 3]
-p a.my_select { |num| num > 1 } # => [2, 3]
-p a.my_select { |num| num == 4 } # => []
+# p a.my_any? { |num| num > 1 } # => true
+# p a.my_any? { |num| num == 4 } # => false
+p a.my_all? { |num| num > 1 } # => false
+p a.my_all? { |num| num < 4 } # => true
 end
